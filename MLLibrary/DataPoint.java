1948755e69452811a3e259bd700569bcbd6b4f10
NathanHaile <56564553+NathanHaile@users.noreply.github.com>
2020-12-09 13:58:14 -0600

    Add files via upload

--- MLLibrary/DataPoint.java
+++ MLLibrary/DataPoint.java
@@ -1,3 +1,5 @@
+
+
 public class DataPoint {
     double f1;
     double f2;
@@ -14,27 +16,35 @@ public class DataPoint {
         f1 = new Random();
         f2 = new Random();
     }
+
 	public double getF1() {
 		return f1;
 	}
+
 	public void setF1(double f1) {
 		this.f1 = f1;
 	}
+
 	public double getF2() {
 		return f2;
 	}
+
 	public void setF2(double f2) {
 		this.f2 = f2;
 	}
+
 	public String getLabel() {
 		return label;
 	}
+
 	public void setLabel(String label) {
 		this.label = label;
 	}
+
 	public String getType() {
 		return type;
 	}
+
 	public void setType(String type) {
 		this.type = type;
 	}
